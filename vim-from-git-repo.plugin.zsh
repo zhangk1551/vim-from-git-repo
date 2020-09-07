b1ef491909ae4c813f444bc4829f3126d72f045c
zhangk1551 <zhangk1551@gmail.com>
2020-09-07 08:43:20 +0800

    clear echo messages;remove 'command' before source

--- vim-from-git-repo.plugin.zsh
+++ vim-from-git-repo.plugin.zsh
@@ -1,1 +1,1 @@
-vg () {command . ${0:A:h}/vim-from-git-repo.zsh "$@"}
+vg () {source ${0:A:h}/vim-from-git-repo.zsh "$@"}
--- vim-from-git-repo.zsh
+++ vim-from-git-repo.zsh
@@ -1,18 +1,14 @@
-#!/bin/bash
+#!/bin/zsh
 
 git_repo_dir=''
 
 findGitRepoDir() {
   p=`/usr/bin/realpath $@`
-  echo $p
-#  while [[ ${p} == */* ]]; do 
   while [[ ! -z ${p} ]]; do
     p=${p%/*}
-    echo $p
     if [ -d ${p}"/.git" ];
     then
       git_repo_dir=$p
-      echo $p
       return
     fi
   done
